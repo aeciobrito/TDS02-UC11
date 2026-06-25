a94cc9b09cc62f8dec322abbbfdf41c4e963e48e
AECIO MENDES BRITO <aeciobrito@gmail.com>
2026-06-25 20:51:13 -0300

    Classe de Testes para Cliente e Endereco

---
+++ IntroducaoTestes/tests/UtilitariosApp.Tests/ClienteTests.cs
@@ -0,0 +1,26 @@
+﻿namespace UtilitariosApp.Tests
+{
+    public class ClienteTests
+    {
+        [Fact]
+        public void Cliente_DeveTerPropriedaesCorretas()
+        {
+            // Arrange
+            var endereco = new Endereco("Rua Tito", 54, "São Paulo", "SP");
+            int id = 87;
+            string nome = "Souza Santos";
+            string email = "souza.santos@mail.com";
+
+            // Act
+            var cliente = new Cliente(id, nome, email, endereco);
+
+            // Assert
+            Assert.NotNull(cliente);
+            Assert.Equal(id, cliente.Id);
+            Assert.Equal(nome, cliente.Nome);
+            Assert.Equal(email, cliente.Email);
+            Assert.NotNull(endereco);
+        }
+
+    }
+}
