3aa8c66cc3211e45364cae8f95ab22eedc3d8c03
AECIO MENDES BRITO <aeciobrito@gmail.com>
2026-06-29 19:12:14 -0300

    update testes

--- IntroducaoTestes/tests/UtilitariosApp.Tests/ClienteTests.cs
+++ IntroducaoTestes/tests/UtilitariosApp.Tests/ClienteTests.cs
@@ -3,7 +3,7 @@
     public class ClienteTests
     {
         [Fact]
-        public void Cliente_DeveTerPropriedaesCorretas()
+        public void Cliente_DeveTerPropriedadesCorretas()
         {
             // Arrange
             var endereco = new Endereco("Rua Tito", 54, "São Paulo", "SP");
@@ -22,5 +22,69 @@
             Assert.NotNull(endereco);
         }
 
+        [Fact]
+        public void Cliente_DeveContribuirEnderecoCorreto()
+        {
+            // Arrange
+            var endereco = new Endereco("Rua Tito", 54, "São Paulo", "SP");
+            int id = 87;
+            string nome = "Souza Santos";
+            string email = "souza.santos@mail.com";
+
+            // Act
+            var cliente = new Cliente(id, nome, email, endereco);
+            var enderecoFormatado = cliente.Enderco.FormatarEndereco();
+
+            // Assert
+            Assert.Equal("Rua Tito, 54, São Paulo, SP", enderecoFormatado);
+        }
+
+        [Fact]
+        public void EmailValido_ComEmailCorreto_DeveRetornarVerdadeiro()
+        {
+            // Arrange
+            var endereco = new Endereco("Rua Tito", 54, "São Paulo", "SP");
+            int id = 87;
+            string nome = "Souza Santos";
+            string email = "souza.santos@mail.com";
+
+            // Act
+            var cliente = new Cliente(id, nome, email, endereco);
+
+            // Assert
+            Assert.True(cliente.EmailValido());
+        }
+
+        [Fact]
+        public void EmailValido_SemArroba_DeveRetornarFalso()
+        {
+            // Arrange
+            var endereco = new Endereco("Rua Tito", 54, "São Paulo", "SP");
+            int id = 87;
+            string nome = "Souza Santos";
+            string email = "souza.santos.mail.com";
+
+            // Act
+            var cliente = new Cliente(id, nome, email, endereco);
+
+            // Assert
+            Assert.False(cliente.EmailValido());
+        }
+
+        [Fact]
+        public void EmailValido_SemPonto_DeveRetornarFalso()
+        {
+            // Arrange
+            var endereco = new Endereco("Rua Tito", 54, "São Paulo", "SP");
+            int id = 87;
+            string nome = "Souza Santos";
+            string email = "souzasantos@mail-com";
+
+            // Act
+            var cliente = new Cliente(id, nome, email, endereco);
+
+            // Assert
+            Assert.False(cliente.EmailValido());
+        }
     }
 }
